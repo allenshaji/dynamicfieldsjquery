15b0545c4c8d0d1d158cfe35a40d0fb9d7b042e4
coder <allenshaji10@gmail.com>
2018-08-17 13:23:27 +0530

    update

--- add.php
+++ add.php
@@ -14,23 +14,25 @@ $value = $_POST['content'];
 
 $array = json_decode($value,true); //json_decode
 
+if (empty($array)) {
+  $response = array(
+			'status' => false,
+		);
+		echo json_encode($response);
+}
+else
+{
 //loop
 foreach($array as $item) {
 	$sql ="INSERT INTO `reg` (Name, Age, Department) 
 	VALUES ('".$item['name']."', '".$item['age']."', '".$item['department']."')";
-	if ($con->query($sql) === TRUE) {
-		$response = array(
+	$con->query($sql);
+		
+	}
+	$response = array(
 			'status' => true,
 		);
 		echo json_encode($response);
 	}
-	else 
-	{
-		$response = array(
-			'status' => false,
-		);
-		echo json_encode($response);
-	}
 
-}
 ?>
